98e5b4c95308e20468a0feb96b65db1736d77427
Harold20231 <00073235@uca.edu.sv>
2026-06-01 17:35:54 -0600

    import arreglado

--- app/src/main/java/com/example/pdm_parcial2/MainActivity.kt
+++ app/src/main/java/com/example/pdm_parcial2/MainActivity.kt
@@ -16,6 +16,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.example.pdm_parcial2.presentation.navigation.NavGraph
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
